04e6834abb6447edfc162e27a1df71e14ed8b414
董方方 <dongfangfang@chanjet.com>
2021-08-15 01:52:23 +0800

    misc:

--- utils/decorator.py
+++ utils/decorator.py
@@ -37,22 +37,23 @@ def Timekeep():
 
 
 def Executor():
-    '''执行shell命令（用于类方法的可调用self的装饰器）
+    '''执行shell命令的类方法装饰器:
+        - 用于类方法 可接收参数self
     '''
-    def wrapper(func):
-        @functools.wraps(func)
+    def wrapper(method):
+        @functools.wraps(method)
         def inner(self, *args, **kwargs):
-            ret = func(self, *args, **kwargs)
+            res = method(self, *args, **kwargs)
             # start_time = time.time()
-            # log.info('Task(%s) start at %s' % (func.__name__,start_time), self.order)
-            # ret = os.popen('ping www.baidu.com')
-            # ret = subprocess.Popen(self.order)
-            # ret = subprocess.run(self.order)
-            # ret = subprocess.Popen(
+            # log.info('Task(%s) start at %s' % (method.__name__,start_time), self.order)
+            # res = os.popen('ping www.baidu.com')
+            # res = subprocess.Popen(self.order)
+            # res = subprocess.run(self.order)
+            # res = subprocess.Popen(
             # self.order, shell=False, stdout=subprocess.PIPE).stdout
             # self.order = ['ffmpeg', '-y', '-loglevel', 'info', '-i', '/Users/nut/Downloads/RS/test.mp4', '-s', '640x360', '-aspect', '640:360', '-threads', '0', '-c:v', 'hevc_videotoolbox', '-r', '24.00', '-pix_fmt', 'yuv420p', '-b:v', '800k', '-maxrate', '1000k', '-bufsize', '4M', '-allow_sw', '1', '-profile:v', 'main', '-vtag', 'hvc1', '-c:a:0', 'aac', '-ac:a:0', '2', '-ar:a:0', '32000', '-b:a:0', '128k', '-strict', '-2', '-sn', '-f', 'mp4', '-map', '0:0', '-map', '0:1', '-map_chapters', '0', '-max_muxing_queue_size', '40000', '-map_metadata', '0', '/Users/nut/Downloads/RS/_compress/test-compress_6.mp4']
             p = subprocess.Popen(self.order, stdout=subprocess.PIPE)
-            # log.info('Executor ret',type(ret),ret.stdout.read(),ret.returncode,ret.stdout,ret.terminate(),ret.wait())
+            # log.info('Executor res',type(res),res.stdout.read(),res.returncode,res.stdout,res.terminate(),res.wait())
             log.debug('Executor waiting...', self.order)
             result = {
                 'returncode': p.wait(),
@@ -65,24 +66,26 @@ def Executor():
             #         # p.returncode
             #         )
             #     print(i)
-            # ret = ret
+            # res = res
             # duration = time.time() - start_time
             log.debug('Executor finish!', result)
-            return dict(ret, **result) if type(ret) == dict else result
+            return dict(res, **result) if type(res) == dict else result
         return inner
     return wrapper
 
 
 def Executor_v2():
-    '''执行shell命令（用于类方法的可调用self的装饰器）
+    '''执行shell命令的类方法装饰器:
+        - 用于类方法 可接收参数self
+        - 对比v1版本的Executor 将添加命令前缀order_prefix_v2的工作在装饰器内做了统一处理
     '''
-    def wrapper(func):
-        @functools.wraps(func)
+    def wrapper(method):
+        @functools.wraps(method)
         def inner(self, *args, **kwargs):
-            ret = func(self, *args, **kwargs)
+            res = method(self, *args, **kwargs)
             order = copy.deepcopy(self.order_prefix_v2)
-            order.extend(ret)
-            order.extend([self.get_output_path(func.__name__)])
+            order.extend(res)
+            order.extend([self.get_output_path(suffix=method.__name__)])
 
             print('order', order)
             p = subprocess.Popen(order, stdout=subprocess.PIPE)
@@ -92,21 +95,29 @@ def Executor_v2():
                 'result': p.communicate()[0],
             }
             log.debug('Executor finish!', result)
-            return dict(ret, **result) if type(ret) == dict else result
+            return dict(res, **result) if type(res) == dict else result
         return inner
     return wrapper
 
 
 def executor(func):
+    '''执行shell命令的函数装饰器
+
+    Arguments:
+        func {[function]} -- [返回终端命令list的函数]
+
+    Returns:
+        [type] -- [description]
+    '''
     @functools.wraps(func)
     def inner(*args, **kwargs):
         order = func(*args, **kwargs)
         p = subprocess.Popen(order, shell=False, stdout=subprocess.PIPE)
-        log.warning('Executor waiting...', order)
+        # log.warning('Executor waiting...', order)
         result = {
             'returncode': p.wait(),
             'result': p.communicate()[0],
         }
-        log.debug('Executor finish!', order, result)
+        # log.debug('Executor finish!', order, result)
         return dict(order, **result) if type(order) == dict else result
     return inner
