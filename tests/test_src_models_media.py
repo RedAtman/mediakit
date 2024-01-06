c0bc3fea488370e1884a51b3f40cd601a9fb1711
redatman <evicmx@gmail.com>
2024-01-07 03:42:03 +0800

    misc: Refactor test class name in test_src_models_media.py

--- tests/test_src_models_media.py
+++ tests/test_src_models_media.py
@@ -4,7 +4,7 @@ from logger import logger
 from src import models
 
 
-class DbEngineTest(unittest.TestCase):
+class MediaTest(unittest.TestCase):
     def setUp(self):
         self.media = models.Media.get_or_create(md5='3a51af5d5e4d3c8b84185729e91e0170')
 
