99baa0b3f5737936ce3dccbb588a8427f697aaea
redatman <evicmx@gmail.com>
2024-01-31 23:21:43 +0800

    refactor: Refactor utils.response module to use ResultStatus and Status classes

--- utils/response.py
+++ utils/response.py
@@ -1,71 +1,88 @@
-from enum import IntEnum, _simple_enum  # type: ignore
-from typing import Any, Optional, Type, Union
+from enum import IntEnum
+from http import HTTPStatus
+from itertools import chain
+from typing import Any, Optional, Union
 
 
-@_simple_enum(IntEnum)
-class ResultStatus:
+class _StatusConstructor(IntEnum):
     value: int
     _value_: int
     phrase: str
     description: str
 
-    def __new__(cls, value: int, phrase: str='', description: str=''):
+    def __new__(cls, value: int, phrase: str = "", description: str = ""):
         obj = int.__new__(cls, value)
         obj._value_ = value
         obj.phrase = phrase
         obj.description = description
         return obj
 
-    # OK = 200, 'OK', 'Success'
-    SUCCESS = 200, 'Success', 'Success'
-    FAILURE = 400, 'Failure'
-    CREATED = 201, 'Created'
-    NOT_FOUND = 404, 'Not Found'
-    NOT_IMPLEMENTED = 501, 'Not Implemented'
-    DATABASE_ERROR = 600, 'Database Error'
+    def __eq__(self, __value: object) -> bool:
+        if __value == self.phrase:
+            return True
+        return super().__eq__(__value)
+
+class ResultStatus(_StatusConstructor):
+    SUCCESS = 0, "Success", "Success"
+    FAILED = 1, "Failed", "Failed"
+    DATABASE_ERROR = 600, "Database Error", "Database Error"
+
+
+Status = _StatusConstructor(
+    "ResultStatus",
+    [
+        (i.name, (i.value, i.phrase, i.description))
+        for i in chain(HTTPStatus, ResultStatus)
+    ],
+)
 
 
 class BaseResponse(dict):
-    STATUS: Type[ResultStatus] = ResultStatus
     __slots__ = (
-        'code',
-        'msg',
-        'data',
+        "code",
+        "msg",
+        "data",
     )
+
     def __new__(cls, *args: Any, **kwargs: Any):
         is_valid_kwargs = set(kwargs.keys()).issubset(cls.__slots__)
-        # print('__new__', cls, args, kwargs, is_valid_kwargs)
         if not is_valid_kwargs:
-            raise KeyError(f'{cls} only accept kwargs: {cls.__slots__}. Got: {args}, {kwargs}')
+            raise KeyError(
+                f"{cls} only accept kwargs: {cls.__slots__}. Got: {args}, {kwargs}"
+            )
         return super().__new__(cls, *args, **kwargs)
 
     def __init__(
-        self, *args: Any,
-        code: int=200,
-        msg: Optional[Union[str, Exception]]=None,
-        data: Optional[Any]=None, **kwargs: Any,
+        self,
+        code: int = 0,
+        msg: Optional[Union[str, Exception]] = None,
+        data: Optional[Any] = None,
+        **kwargs: Any,
     ):
-        code, msg, data = self.check_kwargs(code, msg, data)
-        super().__init__(code=code, msg=msg, data=data)
+        kwargs = self.check_kwargs(code, msg, data)
+        super().__init__(kwargs)
         for attr in self.__slots__:
             setattr(self, attr, self.get(attr, None))
 
     def __setitem__(self, key: str, val: Any):
         if not self.__slots__.__contains__(key):
-            raise KeyError(f"'{self.__class__.__name__}' object has not allowed attribute '{key}'")
+            raise KeyError(
+                f"'{self.__class__.__name__}' object has not allowed attribute '{key}'"
+            )
         super().__setitem__(key, val)
 
     def __eq__(self, __value: object) -> bool:
         return __value in self.values()
 
-    def check_kwargs(self,
-        code: Union[int, ResultStatus]=200,
-        msg: Optional[str]=None,
-        data: Optional[Any]=None, ):
-        if not isinstance(code, ResultStatus):
-            code = self.STATUS(code)
-        msg = msg or code.phrase
-        return code, msg, data
+    def check_kwargs(
+        self,
+        code: Union[int, Status] = 0,
+        msg: Optional[str] = None,
+        data: Optional[Any] = None,
+    ):
+        if isinstance(code, int):
+            code = Status(code)
+        return {"code": code, "msg": msg or code.phrase, "data": data}
 
 
 class Result(BaseResponse):
@@ -77,46 +94,50 @@ class Result(BaseResponse):
         Result(code=100, msg='Hello', data={'a': 1, 'b': 2})
         print(response)
     """
+
     pass
 
 
 class Response(BaseResponse):
 
-    def check_kwargs(self,
-        code: Union[int, ResultStatus]=200,
-        msg: Optional[str]=None,
-        data: Optional[Any]=None, ):
-        code, msg, data = super().check_kwargs(code, msg, data)
+    def check_kwargs(
+        self,
+        code: Union[int, Status] = 0,
+        msg: Optional[str] = None,
+        data: Optional[Any] = None,
+    ):
+        kwargs = super().check_kwargs(code, msg, data)
+        data = kwargs.get("data")
         if not isinstance(data, (dict, list)):
-            raise TypeError(f'{self.__class__} data must be dict or list. Got: {type(data)}')
-        return code, msg, data
+            raise TypeError(
+                f"{self.__class__} data must be dict or list. Got: {type(data)}"
+            )
+        return kwargs
 
 
 if __name__ == "__main__":
-    assert 200 in ResultStatus.__members__.values()
-    print(getattr(ResultStatus, 'SUCCESS'))
-    assert ResultStatus.SUCCESS == 200
-    assert ResultStatus.SUCCESS.value == 200
-    assert ResultStatus.SUCCESS.phrase == 'Success'
-    print(ResultStatus.SUCCESS.description)
-    assert ResultStatus.SUCCESS.description == 'Success'
-    _ = ResultStatus(200)
+    assert 0 in Status.__members__.values()
+    assert 200 in Status.__members__.values()
+    assert Status.SUCCESS == 0
+    assert Status.SUCCESS.value == 0
+    assert Status.SUCCESS.phrase == "Success"
+    assert Status.SUCCESS.description == "Success"
+    _ = Status(0)
     # _ = ResultStatus(200, phrase='abc')
-    assert _ == ResultStatus.SUCCESS
-    assert _ == 200
-    assert _ == ResultStatus.SUCCESS.value
-    assert _.value == 200
-    print(_.phrase, _.description)
-    assert _.phrase == 'Success'
-    assert _.description == 'Success'
     print(type(_), _, _.phrase, _.description)
-    # assert ResultStatus.get_by_value(200) == ResultStatus.OK
-
+    assert _ == 0
+    assert _ == "Success"
+    assert _ == Status.SUCCESS
+    assert _ == Status.SUCCESS.value
+    assert _.value == 0
+    assert _.phrase == "Success"
+    assert _.description == "Success"
     # response = Result()
     response = Result(
-        code=600, msg='abc',
+        code=600,
+        msg="abc",
         # dd=1,
     )
     # response = Result(code=100, msg='Hello', data={'a': 1, 'b': 2})
-    response['data'] = {'c': 1, 'd': 2}
+    response["data"] = {"c": 1, "d": 2}
     print(response)
