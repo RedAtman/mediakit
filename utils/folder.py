1dbb2c0fdeef1d48fe4b6b6fb98c2bc78c6bd3f5
redatman <evicmx@gmail.com>
2024-02-25 20:22:48 +0800

    refactor: Refactor change_file_extension function parameters

--- utils/folder.py
+++ utils/folder.py
@@ -6,17 +6,17 @@ from typing import Any, Dict, Optional
 def change_file_extension(
     folder: Optional[str] = None,
     old_ext: Optional[str] = None,
-    new_ext: Optional[str] = None,
+    ext: Optional[str] = None,
     **kwargs: Any,
 ):
-    if not folder or not old_ext or not new_ext:
-        raise ValueError("folder, old_ext, new_ext must be provided")
+    if not folder or not old_ext or not ext:
+        raise ValueError("folder, old_ext, ext must be provided")
     # Create a case-insensitive glob pattern: samples/*.[mM][pP][44]
     old_ext_pattern = "".join([f"[{c.lower()}{c.upper()}]" for c in old_ext])
     pathname_pattern: str = os.path.join(folder, f"*.{old_ext_pattern}")
     result: Dict[str, Any] = {}
     for filename in glob.glob(pathname_pattern):
         base = os.path.splitext(filename)[0]
-        os.rename(filename, base + f".{new_ext}")
-        result[filename] = base + f".{new_ext}"
+        os.rename(filename, base + f".{ext}")
+        result[filename] = base + f".{ext}"
     return result
