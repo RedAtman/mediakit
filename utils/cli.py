4503d0fd3480e6dae8fd586bf815f80e45ae00d5
redatman <evicmx@gmail.com>
2024-05-03 05:58:47 +0800

    misc: Refactor CLI module to use separate file for argument parsing

---
+++ utils/cli.py
@@ -0,0 +1,66 @@
+import argparse
+import logging
+
+from config import CONFIG
+
+
+logger = logging.getLogger()
+
+
+mapper_action = {
+    "all": None,
+    "compress": None,
+    "trim": None,
+    "scale": None,
+    "change_file_extension": None,
+    "convert_format": None,
+    "save_text": None,
+}
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="""
+        Media Handler CLI;
+
+        e.g.: \r\n
+            %(prog)s compress -t video -w 2;
+        """
+    )
+    parser.add_argument(
+        "action",
+        type=str,
+        choices=mapper_action.keys(),
+        default="all",
+        help="Action to run",
+    )
+    parser.add_argument(
+        "-f",
+        "--folder",
+        type=str,
+        default=CONFIG.MEDIA_FILE_FOLDER,
+        # default=os.getcwd(),
+        help="Folder to run",
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        default="all",
+        choices=("all", "video", "audio", "image"),
+    )
+    parser.add_argument(
+        "-w", "--max_workers", type=int, default=1, help="Number of workers"
+    )
+    parser.add_argument(
+        "-d", "--daemon", type=bool, default=True, choices=(True, False)
+    )
+    # parser.add_argument('-d', '--daemon', nargs="?", const=True)
+    # parser.add_argument('--flag', action='store_true', help='Set the flag value to True')
+    parser.add_argument(
+        "--old_ext", type=str, default=argparse.SUPPRESS, help="Old extension"
+    )
+    parser.add_argument(
+        "--ext", type=str, default=argparse.SUPPRESS, help="New extension"
+    )
+    return parser
