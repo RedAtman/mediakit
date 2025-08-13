2b0a4065bd2eca75f27859084ec248e20c5a9e76
redatman <evicmx@gmail.com>
2025-08-13 22:14:50 +0800

    feat(logger): enhance logging functionality for development environment
    - Add support for multiple log filters in development environment
    - Introduce new log formatter options: 'relpath' and 'json'
    - Implement ColorFilter for colored output in development
    - Update log configuration to use new formatter and filters
    - Improve logging setup for easier debugging and log analysis

--- utils/logger/init.py
+++ utils/logger/init.py
@@ -1,13 +1,27 @@
 import logging
 import logging.config
 import os
+import sys
 
+from . import lexers
 
 ENV = os.getenv("ENV")
-LOG_FORMATTER = "color" if ENV == "development" else "standard"
-LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "logs")
-LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
-LOG_BACKUP_COUNT = os.getenv("LOG_BACKUP_COUNT", 5)
+BASE_DIR: str = os.getenv("BASE_DIR", os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+LOG_DIR: str = os.path.join(BASE_DIR, "logs")
+LOG_LEVEL: str = os.getenv("LOG_LEVEL", "WARNING")
+LOG_BACKUP_COUNT: int = int(os.getenv("LOG_BACKUP_COUNT", 5))
+LOG_FORMATTER = "standard"
+LOG_FILTERS = []
+if ENV == "development":
+    LOG_FORMATTER = "relpath"
+    LOG_FILTERS = [
+        "relpath",
+        "json",
+        "color",
+    ]
+
 
 LOG_CONFIG = {
     "version": 1,
@@ -19,14 +33,18 @@ LOG_CONFIG = {
         "standard": {
             "format": "%(asctime)s:[%(levelname)s]:%(pathname)s:%(lineno)d:%(funcName)s: %(message)s",
         },
-        "color": {
-            "class": "utils.logger.formatters.ColorFormatter",
+        "relpath": {
             "format": "[%(levelname)s]%(relpath)s:%(lineno)d:%(funcName)s: %(message)s",
         },
     },
     "filters": {
         "default": {"()": logging.Filter},
         "relpath": {"()": "utils.logger.filters.RelPathFilter"},
+        "json": {
+            "()": "utils.logger.filters.JsonFilter",
+            "lexer": lexers.json,
+        },
+        "color": {"()": "utils.logger.filters.ColorFilter"},
         "debug": {"()": "utils.logger.filters.LevelMatchFilter", "level": "debug", "operator": "eq"},
         "info": {"()": "utils.logger.filters.LevelMatchFilter", "level": "info", "operator": "eq"},
         "warning": {"()": "utils.logger.filters.LevelMatchFilter", "level": "warning", "operator": "eq"},
@@ -145,8 +163,7 @@ LOG_CONFIG = {
             "class": "logging.StreamHandler",
             "level": "DEBUG",
             "formatter": LOG_FORMATTER,
-            # "formatter": "color",
-            "filters": ["relpath"],
+            "filters": LOG_FILTERS,
         },
     },
     "loggers": {
@@ -184,7 +201,7 @@ LOG_CONFIG = {
 
 
 logging.config.dictConfig(LOG_CONFIG)
-logging.info(f"Logging is configured. ENV: {ENV}, FORMATTER: {LOG_FORMATTER}")
+logging.debug(f"Logging is configured. ENV: {ENV}, FORMATTER: {LOG_FORMATTER}, LOG_FILTERS: {LOG_FILTERS}")
 
 
 if __name__ == "__main__":
